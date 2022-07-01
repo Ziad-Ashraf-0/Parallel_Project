a73d23e3b07a83adc453e289f1f9bb13d434844e
mahmoud abdellatif basiony <66041529+ModyBallba@users.noreply.github.com>
2022-07-02 01:37:15 +0200

    Update AddNewProduct.java

--- Client/src/restaurant/management/system/AddNewProduct.java
+++ Client/src/restaurant/management/system/AddNewProduct.java
@@ -2,9 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package restaurant.management.system;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import model.Category;
 import model.Product;
