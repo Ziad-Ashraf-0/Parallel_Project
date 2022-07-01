81ba42dbfd46ad631584da3d85cc0e3b4e15a163
Khaled amr ismail <72163567+Vnspec2@users.noreply.github.com>
2022-07-01 22:55:09 +0200

    Update OpenPdf.java

--- Server/src/common/OpenPdf.java
+++ Server/src/common/OpenPdf.java
@@ -2,34 +2,4 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package common;
 
-import java.io.File;
-import javax.swing.JOptionPane;
-
-/**
- *
- * @author modyb
- */
-public class OpenPdf {
-    public static void openById(String id){
-    
-    try{
-    if((new File ("C:\\Users\\khaled\\Desktop\\New folder (8)"+id+".pdf")).exists()){          //could be changed
-    Process p=Runtime
-            .getRuntime()
-            .exec("rundll32 url.dll,FileProtocolHandler C:\\Users\\khaled\\Desktop\\New folder (8)"+id+".pdf");
-    
-    }else{
-    JOptionPane.showMessageDialog(null,"File is not Exists");
-    }
-    
-    }
-    catch(Exception e){
-    JOptionPane.showMessageDialog(null,e);
-    
-    }
-    
-    }
-    
-}
