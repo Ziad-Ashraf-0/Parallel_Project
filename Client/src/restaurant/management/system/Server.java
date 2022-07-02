d262ea95bb5a7af2a55081699924e595368efa84
TonyyAdel <85073634+TonyyAdel@users.noreply.github.com>
2022-07-02 08:43:15 +0200

    Update Server.java

--- Client/src/restaurant/management/system/Server.java
+++ Client/src/restaurant/management/system/Server.java
@@ -65,7 +65,15 @@ public class Server {
 
 class ClientHandler implements Runnable {
 
-    
+    public static boolean isNumeric(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
+    }
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<ClientHandler>();
+    private Socket socket;
+    private PrintWriter out;
+    private Scanner in;
+    //private String clientUsername;
 
     public ClientHandler(Socket socket){
         try {
